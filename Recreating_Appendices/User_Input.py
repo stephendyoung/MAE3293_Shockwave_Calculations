831522238838dea2856c5eacfa85bdae69f2ffc8
STEPHEN YOUNG <stephenyoung@STEPHENs-MacBook-Pro.local>
2022-11-17 17:34:33 -0600

    Adding update input file with changes trying to diagnose input problem

--- Recreating_Appendices/User_Input.py
+++ Recreating_Appendices/User_Input.py
@@ -1,5 +1,6 @@
-#from Isentropic_Calculations import RecreatingAppendices
+from Isentropic_Calculations import RecreatingAppendices
+# from MAE3293_Shockwave_Calculations.Recreating_Appendices.Isentropic_Calculations import RecreatingAppendices
 
-
-mach_number = float(input("What is your Mach Number?"))
-
+mach_number = RecreatingAppendices(mymach=float(input("What is your Mach Number ")))
+mach_number.isentropic(mach_number)
+print(f'T0/T =: {mach_number.T_0_over_T}')
