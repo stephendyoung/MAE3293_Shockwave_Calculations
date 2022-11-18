16572a336b2f2b27e622eb28edb0a10ecb1baca5
STEPHEN YOUNG <stephenyoung@STEPHENs-MacBook-Pro.local>
2022-11-18 17:28:18 -0600

    started working on normal shockwave values and added possible ideas for future

--- Recreating_Appendices/Isentropic_Calculations.py
+++ Recreating_Appendices/Isentropic_Calculations.py
@@ -1,5 +1,4 @@
-import math
-class RecreatingAppendices:
+class Appendices:
     def __init__(self, mymach):
         self.mach = mymach
         self.p_0_over_p = None
@@ -7,9 +6,11 @@ class RecreatingAppendices:
         self.T_0_over_T = None
         self.A_over_A_start = None
 
-    def isentropic(self, mymach):
-        self.T_0_over_T = (1 + (1.4 - 1)) / (2 * (mymach ** 2))
+    def isentropic(self):
+        mymach = self.mach
+        self.T_0_over_T = 1 + (((1.4 - 1) / 2) * (mymach ** 2))
         self.p_0_over_p = self.T_0_over_T ** (1.4 / 0.4)
         self.rho_0_over_rho = self.T_0_over_T ** (1 / (1.4 - 1))
-        self.A_over_A_start = 1 / mymach * ((1 + (1.4 - 1) / 2 * mymach ** 2) / ((1.4 + 1) / 2)) ** (
-                    (mymach + 1) / (2 * (mymach - 1)))
+        #self.A_over_A_start = 1 / mymach * ((1 + (1.4 - 1) / 2 * mymach ** 2) / ((1.4 + 1) / 2)) ** (
+                    #(mymach + 1) / (2 * (mymach - 1))) figure out later
+
--- Recreating_Appendices/User_Input.py
+++ Recreating_Appendices/User_Input.py
@@ -1,6 +1,11 @@
-from Isentropic_Calculations import RecreatingAppendices
+from Isentropic_Calculations import Appendices
 # from MAE3293_Shockwave_Calculations.Recreating_Appendices.Isentropic_Calculations import RecreatingAppendices
 
-mach_number = RecreatingAppendices(mymach=float(input("What is your Mach Number ")))
-mach_number.isentropic(mach_number)
-print(f'T0/T =: {mach_number.T_0_over_T}')
+mach_number = Appendices(mymach=float(input("What is your Mach Number ")))
+mach_number.isentropic()  # this calls the isentropic function then the line below calls for each part of the method
+print(f'T0/T = {mach_number.T_0_over_T} P0/P = {mach_number.p_0_over_p} Rho0/Rho = {mach_number.rho_0_over_rho}')
+
+
+""" 
+add normal shock wave, prandtl meyer, and plot table for all three (add possible iteration for previous values. 
+"""
