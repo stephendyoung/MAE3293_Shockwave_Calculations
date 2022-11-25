f9778be77ae6175331e3fa96d2a747642f82c4a0
STEPHEN YOUNG <stephenyoung@STEPHENs-MacBook-Pro.local>
2022-11-24 18:21:38 -0600

    got downstream pressure calculator working

--- Recreating_Appendices/Isentropic_Calculations.py
+++ Recreating_Appendices/Isentropic_Calculations.py
@@ -2,14 +2,14 @@ import math
 
 
 class Appendices:
-    def __init__(self, mymach):  # for solving, need to pass values from user to method
+    def __init__(self, mymach, upstream_static_pressure):  # for solving, need to pass values from user to method
         self.mach = mymach
         self.p_0_over_p = None
         self.rho_0_over_rho = None
         self.T_0_over_T = None
         self.A_over_A_start = None
 
-        # start of normal shock wave values for solving users problems :)
+        # start of normal shock wave appendix
 
         self.p_2_static_over_p_1_static = None
         self.rho_2_static_over_rho_1_static = None
@@ -17,10 +17,12 @@ class Appendices:
         self.ptotal_2_over_ptotal_1 = None
         self.mach_downstream = None
         self.ptotal_2_over_p_1_static = None
-        self.upstream_static_pressure = None
+
+        # start of normal shock waves calculator
+        self.given_static_pressure = upstream_static_pressure
+        self.downstream_static_pressure = None
 
         # expansion fan equations
-
         self.mu = None
         self.nu = None
 
@@ -34,7 +36,7 @@ class Appendices:
         # self.A_over_A_start = 1 / mymach * ((1 + (1.4 - 1) / 2 * mymach ** 2) / ((1.4 + 1) / 2)) ** (
         # (mymach + 1) / (2 * (mymach - 1))) figure out later
 
-    def normal_shcokwave(self):
+    def normal_shockwave(self):
         Appendices.isentropic(self)
         mymach = self.mach
         self.p_2_static_over_p_1_static = 1 + (((2 * 1.4) / (1.4 + 1)) * ((mymach ** 2) - 1))
@@ -51,8 +53,6 @@ class Appendices:
         self.nu = math.degrees(math.sqrt((1.4 + 1) / (1.4 - 1)) * math.atan(
             math.sqrt(((1.4 - 1) / (1.4 + 1)) * ((mymach ** 2) - 1))) - math.atan(math.sqrt(mymach ** 2 - 1)))
 
-    """ def normal_shock_calculations(self):
-        mymach = self.mach
-        upstream_static_pressure = self.upstream_static_pressure
-        downstream_static_pressure = upstream_static_pressure * (1/ self.)
-        """
+    def normal_shock_calculations(self):
+        upstream_static_pressure = self.given_static_pressure
+        self.downstream_static_pressure = upstream_static_pressure * self.p_2_static_over_p_1_static
--- Recreating_Appendices/User_Input.py
+++ Recreating_Appendices/User_Input.py
@@ -1,9 +1,11 @@
 from Isentropic_Calculations import Appendices
+
 # from MAE3293_Shockwave_Calculations.Recreating_Appendices.Isentropic_Calculations import RecreatingAppendices
 user_selection = input("What appendix are you looking for? \n A) Isentropic Relationship \n B) Normal Schockwave "
                        "Relationship \n C) Prandtl-Meyer Relationship\n Enter A, B, or C: ")
 
-mach_number = Appendices(mymach=float(input("\nWhat is your Mach Number ")))
+mach_number = Appendices(mymach=float(input("\nWhat is your Mach Number ")), upstream_static_pressure=float(input("\nWhat is the upstream static pressure? ")))
+# upstream_static_user = Appendices(upstream_static_pressure=float(input("\nWhat is the upstream static pressure? ")))
 
 """ line above calls the isentropic function then the line below calls for each part of the method. 
 error within was dealing with passing the mach number twice but once you assign it, the entire class knows it already. 
@@ -12,25 +14,27 @@ Therefore, don't pass it again in class file- comment in there for it.
 
 if user_selection == "A":
     mach_number.isentropic()
-    print(f'T0/T = {mach_number.T_0_over_T:.3f} P0/P = {mach_number.p_0_over_p:3f} Rho0/Rho = {mach_number.rho_0_over_rho:4f}')
+    print(
+        f'T0/T = {mach_number.T_0_over_T:.3f} P0/P = {mach_number.p_0_over_p:3f} Rho0/Rho = {mach_number.rho_0_over_rho:4f}')
 elif user_selection == "B":
-    mach_number.normal_shcokwave()
+    mach_number.normal_shockwave()
     mach_number.isentropic()
 
-    print(f'P_2/P_1 = {mach_number.p_2_static_over_p_1_static:.3f} Rho_2/Rho_1 {mach_number.rho_2_static_over_rho_1_static:.3f}'
-          f' T_2/T_1 = {mach_number.T_2_over_T_1:.3f} Ptotal_2/Ptotal_1 {mach_number.ptotal_2_over_ptotal_1:.3f}'
-          f' Ptotal_2/P_1 = {mach_number.ptotal_2_over_p_1_static:.3f} Downstream Mach Number'
-          f' {mach_number.mach_downstream:.4f}')
+    print(
+        f'P_2/P_1 = {mach_number.p_2_static_over_p_1_static:.3f} Rho_2/Rho_1 {mach_number.rho_2_static_over_rho_1_static:.3f}'
+        f' T_2/T_1 = {mach_number.T_2_over_T_1:.3f} Ptotal_2/Ptotal_1 {mach_number.ptotal_2_over_ptotal_1:.3f}'
+        f' Ptotal_2/P_1 = {mach_number.ptotal_2_over_p_1_static:.3f} Downstream Mach Number'
+        f' {mach_number.mach_downstream:.4f}')
 
-
-
+    mach_number.normal_shock_calculations()
+    print(f'\nDownstream Static Pressure (P2) = {mach_number.downstream_static_pressure:.3f}')
 
 elif user_selection == "C":
     mach_number.expansion_fans()
     print(f'mu = {mach_number.mu:.2f}degrees nu = {mach_number.nu:.3f}')
 
 """ 
-add normal shock wave (done), prandtl meyer, and plot table for all three (add possible iteration for previous values.) 
+add normal shock wave (done), prandtl meyer (done), and plot table for all three (add possible iteration for previous values.) 
 also add what is in notes app Dr. Kara talked about. Can also add try and except to take keyboard interruption/cancel 
 key
 """
