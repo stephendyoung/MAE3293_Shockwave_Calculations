639e21eeb968232d5eea45d4b3b71e6e8e9e78ad
STEPHEN YOUNG <stephenyoung@stephens-mbp.eduroam-corewlcs-13.okstate.edu>
2022-11-28 12:32:41 -0600

    fixed problem with tabulating tables

--- Recreating_Appendices/Tabulation_of_Tables.py
+++ Recreating_Appendices/Tabulation_of_Tables.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 data = []
 mymach = 0.02
 while mymach < 1.02:
-    mach_number = Appendices(mymach)
+    mach_number = Appendices(mymach, upstream_static_pressure= None)
     mach_number.isentropic()
     data.append(f' Mach Number: {mymach:.2f} T0/T = {mach_number.T_0_over_T:.3f} P0/P = {mach_number.p_0_over_p:3f} Rho0/Rho ='
                 f' {mach_number.rho_0_over_rho:4f}')
